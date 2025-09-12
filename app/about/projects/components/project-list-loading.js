aaf4deb58cef555f2cec470c34242f9b538e49a7
umasaipavan <umasaipavantanukula@gmail.com>
2025-09-12 16:22:16 +0530

    few changes

--- app/about/projects/components/project-list-loading.js
+++ app/about/projects/components/project-list-loading.js
@@ -1,11 +1,15 @@
-export default function ProjectListLoading() {
+export default function Loading() {
   return (
-    <ul className="space-y-8">
-      {Array(3).fill(0).map((_el, index) => (
-        <li key={index}>
-          <div className="w-full h-24 animate-pulse bg-neutral-100 dark:bg-neutral-700"></div>
-        </li>
-      ))}
-    </ul>
+    <div className="p-20">
+      <h1 className="mb-8 text-xl">Projects</h1>
+
+      <ul className="space-y-8">
+        {Array(3).fill(0).map((_el, index) => (
+          <li key={index}>
+            <div className="w-full h-24 animate-pulse bg-neutral-100 dark:bg-neutral-700"></div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
