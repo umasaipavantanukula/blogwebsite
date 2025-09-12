b6d8fdb89079bbbde1750ef4866a2736efb5a810
umasaipavan <umasaipavantanukula@gmail.com>
2025-09-12 18:05:54 +0530

    few

--- app/about/projects/components/project-list-loading.js
+++ app/about/projects/components/project-list-loading.js
@@ -1,15 +1,11 @@
-export default function Loading() {
+export default function ProjectListLoading() {
   return (
-    <div className="p-20">
-      <h1 className="mb-8 text-xl">Projects</h1>
-
-      <ul className="space-y-8">
-        {Array(3).fill(0).map((_el, index) => (
-          <li key={index}>
-            <div className="w-full h-24 animate-pulse bg-neutral-100 dark:bg-neutral-700"></div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="space-y-8">
+      {Array(3).fill(0).map((_el, index) => (
+        <li key={index}>
+          <div className="w-full h-24 animate-pulse bg-neutral-100 dark:bg-neutral-700"></div>
+        </li>
+      ))}
+    </ul>
   )
 }
--- app/about/projects/components/project-list.js
+++ app/about/projects/components/project-list.js
@@ -1,6 +1,4 @@
-// export const dynamic = 'force-dynamic'
-
-export default async function ProjectsPage() {
+export default async function ProjectList() {
   const response = await fetch(
     'http://localhost:3001/repos',
     { cache: 'no-store' }
@@ -8,18 +6,14 @@ export default async function ProjectsPage() {
   const repos = await response.json()
 
   return (
-    <div className="p-20">
-      <h1 className="mb-8 text-xl">Projects</h1>
-
-      <ul>
-        {repos.map(repo => (
-          <li key={repo.id} className="mb-4">
-            <div>{repo.title}</div>
-            <div>{repo.description}</div>
-            <div>{repo.stargazers_count}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {repos.map(repo => (
+        <li key={repo.id} className="mb-4">
+          <div>{repo.title}</div>
+          <div>{repo.description}</div>
+          <div>{repo.stargazers_count}</div>
+        </li>
+      ))}
+    </ul>
   )
 }
